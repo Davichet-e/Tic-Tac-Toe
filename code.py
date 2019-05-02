4ccf4414a44d51f2edbb34b44cd38981765ba9a9
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-05-02 23:02:33 +0200

    Improved the reset() function and fixed some bugs

--- code.py
+++ code.py
@@ -1,56 +1,86 @@
-i = 0
+"""
+    TODO
+"""
 
 
-def tic_tac_toe():
-    global i
+def tic_tac_toe() -> None:
+    counter: int = 0
 
-    def win_condition(symbol):
-        return ((board[15] == board[21] == board[27] == symbol)
-                or (board[49] == board[55] == board[61] == symbol)
-                or (board[83] == board[89] == board[95] == symbol)
-                or (board[15] == board[49] == board[83] == symbol)
-                or (board[21] == board[55] == board[89] == symbol)
-                or (board[27] == board[61] == board[95] == symbol)
-                or (board[15] == board[55] == board[95] == symbol)
-                or (board[27] == board[55] == board[83] == symbol))
+    def win_condition(symbol) -> bool:
+        return (
+            board[15] == board[21] == board[27] == symbol
+            or board[49] == board[55] == board[61] == symbol
+            or board[83] == board[89] == board[95] == symbol
+            or board[15] == board[49] == board[83] == symbol
+            or board[21] == board[55] == board[89] == symbol
+            or board[27] == board[61] == board[95] == symbol
+            or board[15] == board[55] == board[95] == symbol
+            or board[27] == board[55] == board[83] == symbol
+        )
 
-    def reset():
-        global i
-        rematch = input("Do you want to play again? (y/n)\n")
-        if rematch == "y" or rematch == "yes":
-            i = 0
+    def reset() -> bool:
+        nonlocal counter
+        rematch: str = input("Do you want to play again? (y/n)\n")
+        if rematch in ("y", "yes"):
+            counter = 0
             tic_tac_toe()
             return True
         else:
             print("Goodbye! Thanks for playing.")
+            return False
 
-    def turn(symbol):
-        global i
-        selection = input('Select box:')
+    def turn(symbol: str) -> None:
+        nonlocal counter
+        selection: str = input("Select box:").strip().lower()
         try:
             if board[sel_dict[selection]] == " ":
                 board[sel_dict[selection]] = symbol
-                i += 1
+                counter += 1
             else:
                 print("Box already selected!")
 
         except KeyError:
-            print('Invalid box!')
-        print_board = ''
+            print("Invalid box!")
+        print_board: str = ""
         for x in board:
             print_board += x
         print(print_board)
 
     print("Welcome to Tic-Tac-Toe!")
-    boar = '     |     |\n     |     |   \n-----|-----|-----\n     |     |   \n-----|-----|-----\n     |     |   \n     |     |'
-    board = [x for x in boar]
-    sel_dict = {'a1': 15, 'a2': 21, 'a3': 27, 'b1': 49,
-                'b2': 55, 'b3': 61, 'c1': 83, 'c2': 89, 'c3': 95}
-    select_board = '     |     |\n a1  | a2  | a3 \n-----|-----|-----\n b1  | b2  | b3\n-----|-----|-----\n c1  | c2  | c3\n     |     |'
+    boar: str = (
+        "     |     |\n"
+        "     |     |   \n"
+        "-----|-----|-----\n"
+        "     |     |   \n"
+        "-----|-----|-----\n"
+        "     |     |   \n"
+        "     |     |"
+    )
+    board: list = [x for x in boar]
+    sel_dict: dict = {
+        "a1": 15,
+        "a2": 21,
+        "a3": 27,
+        "b1": 49,
+        "b2": 55,
+        "b3": 61,
+        "c1": 83,
+        "c2": 89,
+        "c3": 95,
+    }
+    select_board: str = (
+        "     |     |\n"
+        " a1  | a2  | a3 \n"
+        "-----|-----|-----\n"
+        " b1  | b2  | b3\n"
+        "-----|-----|-----\n"
+        " c1  | c2  | c3\n"
+        "     |     |"
+    )
     print(select_board)
 
-    while i < 9:
-        if i % 2 == 0:
+    while counter < 9:
+        if counter % 2 == 0:
             turn("x")
             if win_condition("x"):
                 print("Congratulations, Player1 won!")
@@ -62,7 +92,7 @@ def tic_tac_toe():
                 print("Congratulations, Player2 won!")
                 reset()
                 break
-        if i == 9:
+        if counter == 9:
             print("It is a tie!")
             reset()
             break
